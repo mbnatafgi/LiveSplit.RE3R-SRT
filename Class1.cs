c7edf89ea4e4878f2d76f8f758689cf7450aaab8
Mohamad Belal Natafgi <bilal.natafji@gmail.com>
2020-12-26 00:55:46 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit.RE3R_SRT
+{
+    public class Class1
+    {
+    }
+}
